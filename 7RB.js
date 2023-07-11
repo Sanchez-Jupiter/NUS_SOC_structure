48c6376d1f94b8a9d366986640f0266e39022054
Source Academy User <No public email provided>
2023-07-11 16:43:04 +0800

    Changes made from Source Academy

--- 7RB.js
+++ 7RB.js
@@ -1,8 +1,13 @@
 
 function make_withdraw(balance, my_password) {
     
+    let left_times = 3;
     function withdraw(amount, password) {
-        if(my_password !== password){
+        if(left_times === 0) {
+            return "Account disabled";
+        } 
+        else if(my_password !== password){
+            left_times = left_times - 1;
             return "Wrong password; no withdraw";
         }
         else if (balance >= amount) {
@@ -16,6 +21,3 @@ function make_withdraw(balance, my_password) {
 }
 
 const W1 = make_withdraw(100, "123");
-W1(40, "123");
-W1(40, "12");
-W1(40, "1");
